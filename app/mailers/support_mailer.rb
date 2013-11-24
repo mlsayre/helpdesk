7f46027a25a32832fce2333686db60e7ada8a8c6
Matt Sayre <mlsayre@gmail.com>
2013-11-23 16:28:15 -0800

    send fix

--- app/mailers/support_mailer.rb
+++ app/mailers/support_mailer.rb
@@ -2,6 +2,8 @@ class SupportMailer < ActionMailer::Base
   default from: "matt@thegamecomposer.com"
 
   def ticket_notify
+    @ticket = Ticket.find(params[:id])
+
     mail to: "matt@thegamecomposer.com", subject: @ticket.name
   end
 end
