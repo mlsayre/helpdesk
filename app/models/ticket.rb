a0d1895e0d832ad05384fb5cbe28474c22b00b65
Matt Sayre <mlsayre@gmail.com>
2013-11-24 14:25:07 -0800

    inbound again

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,9 +2,8 @@ class Ticket < ActiveRecord::Base
   attr_accessible :name, :problem, :reply
 
   def self.create_from_postmark(mitt)
-    api_token = mitt.to.split("@").first.split("+").last
 
-      ticket = ticket.new
+      ticket = tickets.new
       ticket.name = mitt.subject
       ticket.reply = if mitt.text_body.blank?
         mitt.html_body
