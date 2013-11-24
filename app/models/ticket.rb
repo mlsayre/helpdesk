d419723bbd6784f053454b2576f6ebcf20e32bf9
Matt Sayre <mlsayre@gmail.com>
2013-11-24 15:23:07 -0800

    email server created

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,14 +3,14 @@ class Ticket < ActiveRecord::Base
 
   def self.create_from_postmark(mitt)
 
-      @ticket = Ticket.new
-      @ticket.name = mitt.subject
-      @ticket.reply = if mitt.text_body.blank?
+      ticket = tickets.new
+      ticket.name = mitt.subject
+      ticket.reply = if mitt.text_body.blank?
         mitt.html_body
       else
         mitt.text_body
       end
       # post.photo = mitt.attachments.first.read unless mitt.attachments.empty?
-      @ticket.save
+      ticket.save
   end
 end
