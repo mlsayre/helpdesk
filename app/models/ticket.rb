7f41e0fbe4f37eb1bc4529d4f7e2cfbb00fb3b78
Matt Sayre <mlsayre@gmail.com>
2013-11-24 16:10:35 -0800

    new approach model

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,16 +1,13 @@
 class Ticket < ActiveRecord::Base
   attr_accessible :name, :problem, :reply
 
-  def self.create_from_postmark(mitt)
-
-      ticket = Ticket.new
-      ticket.name = mitt.subject
-      ticket.reply = if mitt.text_body.blank?
-        mitt.html_body
-      else
-        mitt.text_body
-      end
-      # post.photo = mitt.attachments.first.read unless mitt.attachments.empty?
-      ticket.save
+  def self.create_from_inbound_hook(message)
+    self.new(:reply => message["TextBody"],
+             :name => message["Subject"])
   end
+
+post '/inbound' do
+  request.body.rewind
+  ticket = Ticket.create_from_inbound_hook(Postmark::Json.decode(request.body.read))
+  logger.info comment.inspect
 end
