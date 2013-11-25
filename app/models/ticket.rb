41243ae7e20490cf052ccdf85abb4d231a5a1246
Matt Sayre <mlsayre@gmail.com>
2013-11-24 18:30:20 -0800

    create to update

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,9 +2,9 @@ class Ticket < ActiveRecord::Base
   attr_accessible :name, :problem, :reply
 
   def self.create_from_inbound_hook(message)
-    self.new(:id => message["Subject"],
-             :reply => message["TextBody"]
-             )
+    self.update_attributes(:id => message["Subject"],
+                           :reply => message["TextBody"]
+                           )
   end
 end
 
