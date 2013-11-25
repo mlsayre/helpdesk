e03ef3523b3ea838b6772381f36fc35a6a55ef6f
Matt Sayre <mlsayre@gmail.com>
2013-11-24 18:51:57 -0800

    testing

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,9 +2,10 @@ class Ticket < ActiveRecord::Base
   attr_accessible :name, :problem, :reply
 
   def self.create_from_inbound_hook(message)
-    ticket = Ticket.find(:id)
 
-    ticket.update_attributes(:id => message["Subject"], :reply => message["TextBody"])                           )
+    self.update_attributes(:id => message["Subject"],
+                           :reply => message["TextBody"]
+                           )
   end
 end
 
